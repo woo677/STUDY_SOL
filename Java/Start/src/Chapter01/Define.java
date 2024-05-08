cda1afc883838e6695dee67af40b8a6a8282c4ca
min_woo <minwoo11670@gmail.com>
2024-05-08 14:18:28 +0900

    변수 기본 타입, 명시적 형변환

--- Java/Start/src/Chapter01/Define.java
+++ Java/Start/src/Chapter01/Define.java
@@ -2,9 +2,22 @@ package Chapter01;
 
 public class Define {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) { // 이게 없으면 프로그램이 스스로 실행되지 않는다
+		/*
+		 * 여러줄 주석 여러 줄을 사용 할때 쓴다.
+		 */
 
+//		기본 타입들
+
+		boolean varBool = true;
+		String varStr = "문자열"; // 여러 글자 (버블쿼터)
+		char varChar = 'S'; // 한글자(싱글쿼터)
+
+		int varInt = 0; // 일반적인 정수
+		long varLong = 1000000000; // 큰 정수
+
+		double varDouble = 0.0001;// 큰 소숫점(일반적으로 소숫점 5자리 이상)
+		float varFloat = 0.1f; // float 는 특수하게 f 를 붇여 준다.
 	}
 
 }
